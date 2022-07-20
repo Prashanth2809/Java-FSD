fc82373f271b38f371292acea7a387a22694087f
Prashanth2809 <109527867+Prashanth2809@users.noreply.github.com>
2022-07-20 19:38:02 +0530

    Add files via upload

---
+++ Validationemail.java
@@ -0,0 +1,29 @@
+package validationemail;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class Validationemail {
+	public static void main(String[] args) {
+		String regex="^(.+)@(.+)$";
+		String[]strarray=new String[8];
+		Scanner Str=new Scanner(System.in);
+		System.out.println("Enter email address");
+		String a=Str.nextLine();
+		
+		
+		Pattern p=Pattern.compile(regex);
+		Matcher m=p.matcher(a);
+		
+		if(m.matches()) {
+			System.out.println("Entered mail is valid");
+		}
+		else {
+			System.out.println("not valid");
+		}
+		
+
+	}
+	
+}
+
+
